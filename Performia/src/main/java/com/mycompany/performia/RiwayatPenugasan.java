b004953310773a689d94ec2a992f0320df30178f
Zefanya Pijar Manullang <zpijarmanullang@gmail.com>
2026-06-11 10:51:32 +0700

    Update RiwayatPenugasan.java
    halo arthes

--- Performia/src/main/java/com/mycompany/performia/RiwayatPenugasan.java
+++ Performia/src/main/java/com/mycompany/performia/RiwayatPenugasan.java
@@ -1,6 +1,7 @@
-*
+/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this templates
+ * Halo dari zefah
  */
 package com.mycompany.performia;
 
